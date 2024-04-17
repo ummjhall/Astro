b17e11623a173fe87dde99ab35390fb97cfba821
Justin Hall <justinhall.jnh@gmail.com>
2024-04-18 02:21:18 +0900

    Backend > product_image_routes: Implement Delete Product Image

--- app/api/product_image_routes.py
+++ app/api/product_image_routes.py
@@ -58,3 +58,47 @@ def add_image(product_id):
     db.session.commit()
 
     return new_image.to_dict(), 201
+
+
+@product_image_routes.route('/<product_id>', methods=['DELETE'])
+@login_required
+def delete_image(product_id):
+    """
+    Deletes a product image specified by id.
+    """
+    req = request.json
+    product = Product.query.get(product_id)
+
+    # Error response: Product couldn't be found
+    if not product:
+        return {'message': "Product couldn't be found"}, 404
+
+    # Error response: Product does not belong to the current user
+    if product.seller_id != current_user.id:
+        return {'message': 'Forbidden'}, 403
+
+    # Error response: Body validation errors
+    errors = {}
+    if 'image_id' not in req:
+        errors['image_id'] = 'Image id required'
+    else:
+        if not isinstance(req['image_id'], int) or req['image_id'] < 1:
+            errors['image_id'] = 'Invalid image id'
+    if errors:
+        return {
+            'message': 'Bad Request',
+            'errors': errors
+        }, 400
+
+    # Error response: Image couldn't be found
+    found = list(filter(lambda image: image.id==req['image_id'], product.product_images))
+    if not found:
+        return {'message': "Image couldn't be found"}, 404
+
+    # SUCCESS
+    image = found[0]
+
+    db.session.delete(image)
+    db.session.commit()
+
+    return {'message': 'Successfully deleted'}

3718a67897feab44d9023e7e04006ce1f5e14748
Justin Hall <justinhall.jnh@gmail.com>
2024-04-20 18:31:36 +0900

    Frontend > Products: Update ProductDetails component

--- react-vite/src/components/Products/ProductDetails.jsx
+++ react-vite/src/components/Products/ProductDetails.jsx
@@ -1,12 +1,21 @@
-// import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getProductDetailsThunk } from '../../redux/products';
 import './products.css';
 
 function ProductDetails() {
-  // const { productId } = useParams();
+  const products = useSelector(state => state.products);
+  const dispatch = useDispatch();
+  const { productId } = useParams();
+
+  useEffect(() => {
+    dispatch(getProductDetailsThunk(productId));
+  }, [dispatch]);
 
   return (
     <div className='productdetails-wrapper'>
-      ProductDetails
+      {products[productId].name}
     </div>
   );
 }

8ae3600fd7effea092be5cf105e64ded8eb8fd8e
Justin Hall <justinhall.jnh@gmail.com>
2024-05-11 18:15:34 +0900

    Frontend: Add and implement SaleCard component

--- react-vite/src/components/Home/Home.jsx
+++ react-vite/src/components/Home/Home.jsx
@@ -2,14 +2,16 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllProductsThunk } from '../../redux/products';
 import { useNavigate } from 'react-router-dom';
-import FeaturedProduct from './FeaturedProduct';
+import SaleCard from './SaleCard';
 import ProductTeaser from './ProductTeaser';
+import FeaturedProduct from './FeaturedProduct';
 import './home.css';
 
 function Home() {
   const allProducts = useSelector(state => state.products);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const saleItems = [50, 34, 22, 48, 67];
   const techTeasers = [10, 12, 13, 14, 16];
 
 
@@ -23,6 +25,11 @@ function Home() {
       <div className='home-container'>
         <div className='home-box home-sales'>
           <div className='home-heading'>ASTRO SALES</div>
+          <div className='home-salecard-container'>
+            {saleItems.map(productId => (
+              <SaleCard key={productId} product={allProducts[productId]} />
+            ))}
+          </div>
         </div>
         <div className='home-box home-latest' onClick={() => navigate('/products/tech')}>
           <div className='home-heading'>GET THE LATEST IN TECH</div>

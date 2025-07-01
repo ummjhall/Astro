6d608eb70f5476ce021400645ee7857e4597a892
Justin Hall <justinhall.jnh@gmail.com>
2025-07-01 19:31:32 +0900

    Frontend > Home: Fix css on featured product and 'latest' tiles

--- react-vite/src/components/Home/FeaturedProduct.jsx
+++ react-vite/src/components/Home/FeaturedProduct.jsx
@@ -12,7 +12,12 @@ function FeaturedProduct({ product }) {
 
   return product && (
     <div className='featured-wrapper' onClick={handleClick}>
-      <img className='featured-image' src={product.previewImage} style={{width: '100px', height: '100px'}} />
+      <img
+        className='featured-image'
+        src={product.previewImage}
+        width={100}
+        height={100}
+      />
       <div className='featured-info-container'>
         <div className='featured-name'>{product.name}</div>
         <div className='featured-price'>ঋ {product.price.toLocaleString()}</div>

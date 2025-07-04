9ff50f479fa0049154426e48928a0c6199828c4f
Justin Hall <justinhall.jnh@gmail.com>
2025-07-04 15:03:10 +0900

    Frontend > Home: Fix css on currency tile

--- react-vite/src/components/Home/Home.jsx
+++ react-vite/src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
 
   const saleItems = [50, 34, 22, 48, 67];
   const techTeasers = [10, 12, 13, 14, 16];
-  const currencies = ['algo', 'cfx', 'jpy', 'mkd', 'zil'];
+  const currencies = ['algo', 'cfx', 'jpy', 'mkd', 'zil', 'flr', 'ugx', 'lkr', 'pyg', 'sos', 'rvn', 'mur', 'gmd', 'egp', 'amp'];
 
 
   useEffect(() => {
